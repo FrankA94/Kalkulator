978b35ef4790b8b13b892f0a4803f885fbaf78b3
FrankA94 <96749300+FrankA94@users.noreply.github.com>
2022-05-06 10:33:58 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Kalkulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Skriv inn et tall: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Svriv inn enda et tall: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
+            Console.WriteLine(num1 - num2);
+            Console.WriteLine(num1 / num2);
+            Console.WriteLine(num1 * num2);
+            Console.ReadLine();
+        }
+    }
+}
